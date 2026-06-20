5ab8f7fe2bc0ef7a9505959683535ed6f53eb479
GameFunns98 <dudavasek76@gmail.com>
2026-06-20 17:32:55 +0200

    app run

--- app.py
+++ app.py
@@ -577,4 +577,4 @@ app.jinja_env.globals.update(
 if __name__ == "__main__":
     with app.app_context():
         init_db()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
